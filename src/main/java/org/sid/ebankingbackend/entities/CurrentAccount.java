55734d31d67c20080d8f4892395b8089afed5b36
zakariasaadoun <saadoun.zakaria.dev@gmail.com>
2024-06-19 19:13:35 +0100

    Commit

---
+++ src/main/java/org/sid/ebankingbackend/entities/CurrentAccount.java
@@ -0,0 +1,17 @@
+package org.sid.ebankingbackend.entities;
+
+import jakarta.persistence.DiscriminatorValue;
+import jakarta.persistence.Entity;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.NonNull;
+
+
+
+@Entity
+@DiscriminatorValue("CA")
+@Data @NoArgsConstructor @AllArgsConstructor
+public class CurrentAccount extends BankAccount {
+    private double overDraft;
+}
